2a67e5ff032070a07102bbd4792e420323954bc2
Joem <josephemmanuel.petilla@gmail.com>
2019-01-11 11:03:05 +0800

    + added more demo apis

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+/**
+ * Copyright 2019, Wizy, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START terega_idam_thirdparty_api_demo]
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// Start the server and will listen to port 5000. See line 19
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// [END terega_idam_thirdparty_api_demo]
